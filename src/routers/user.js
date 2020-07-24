fc679fd38535dac0b5fa39a4d611e2e7687e5ae0
swapnil042 <sheikhswapnil.musa@gmail.com>
2020-07-25 01:47:44 +0600

    Complete Picture Upload feature

---
+++ src/routers/user.js
@@ -0,0 +1,109 @@
+const express = require('express')
+const User = require('../models/user')
+const multer = require('multer')
+const auth = require('../middleware/auth')
+const router = new express.Router()
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.post('/users/logout',auth,async (req,res)=>{
+    try{
+        req.user.tokens = req.user.tokens.filter((token)=> token.token !== req.token) 
+        await req.user.save()
+        res.status(200).send()
+    }catch(e){
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res)=>{
+    try{
+        req.user.tokens= []
+        await req.user.save()
+        res.status(200).send()
+    }catch(e){
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+
+router.patch('/users/me',auth, async (req, res) => {
+    const user = req.user
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/users/me',auth , async (req, res) => {
+    try {
+        await req.user.remove()
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+const upload = multer({
+    limits:{
+        fieldSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('Please upload an img'))
+        }
+
+        cb(undefined, true) //accepting uploads
+    }
+})
+
+router.post('/user/me/avatar', auth,upload.single('avatar'), async (req,res)=>{
+    req.user.avatar = req.file.buffer   
+    await req.user.save()
+    res.send()
+}, (err, req, res, next)=>{
+    res.status(400).send({error: err.message})
+})
+
+router.delete('/user/me/avatar', auth, async (req,res)=>{
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+module.exports = router
